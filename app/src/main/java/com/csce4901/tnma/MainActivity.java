86c8132df903607753898193b4e34c846944666d
Anil Poudel <anil.pdl12@gmail.com>
2020-03-14 02:00:27 -0500

    fixed issues with login and signup

--- app/src/main/java/com/csce4901/tnma/MainActivity.java
+++ app/src/main/java/com/csce4901/tnma/MainActivity.java
@@ -2,8 +2,9 @@ package com.csce4901.tnma;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.transition.TransitionManager;
@@ -19,7 +20,7 @@ import com.google.android.material.tabs.TabLayout;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements LoginTab.OnFragmentInteractionListener, SignupTab.OnFragmentInteractionListener{
 
 //    Variables For Signup Transitions
 //    TextView txt1;
@@ -41,7 +42,7 @@ public class MainActivity extends AppCompatActivity{
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
@@ -61,29 +62,6 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
-
-//        //Transitions for Signup tab
-//        btn1 = (Button)findViewById(R.id.confirm_email);
-//        btn2 = (Button)findViewById(R.id.verify);
-//        btn3 = (Button)findViewById(R.id.create_account);
-//
-//        txt1 = (TextView)findViewById(R.id.text001);
-//        edit1 = (EditText)findViewById(R.id.verification);
-//        edit2 = (EditText)findViewById(R.id.passcode1);
-//        edit3 = (EditText)findViewById(R.id.passcode2);
-//        //
-//
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//              txt1.setVisibility(View.VISIBLE);
-//              edit1.setVisibility(View.VISIBLE);
-//              btn2.setVisibility(View.VISIBLE);
-//              Toast.makeText(getApplicationContext(),"A verification code has been sent" +
-//                        "to your email address.", Toast.LENGTH_LONG).show();
-//            }
-//        });
-//
 
         }
 
--- app/src/main/java/com/csce4901/tnma/SignupTab.java
+++ app/src/main/java/com/csce4901/tnma/SignupTab.java
@@ -14,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.csce4901.tnma.Connector.FirebaseConnector;
@@ -28,8 +30,12 @@ public class SignupTab extends Fragment {
     private Unbinder unbinder;
     private FirebaseAuth firebaseAuth;
     FirebaseConnector fbConnector = new FirebaseConnector();
+
     @BindView(R.id.emailSignup) EditText userEmail;
     @BindView(R.id.passcode1) EditText userPass;
+    @BindView(R.id.passcode2) EditText userPassConfirm;
+
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,31 +70,50 @@ public class SignupTab extends Fragment {
         firebaseAuth = fbConnector.getFirebaseAuthInstance();
         String user = userEmail.getText().toString();
         String pass = userPass.getText().toString();
-        firebaseAuth
-                .createUserWithEmailAndPassword(user, pass)
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(context,
-                                "Registration successful!",
-                                Toast.LENGTH_LONG)
-                                .show();
+        String passConfirm = userPassConfirm.getText().toString();
 
-                        // if the user created intent to login activity
-                        Intent intent
-                                = new Intent(context,
-                                MainActivity.class);
-                        startActivity(intent);
-                    } else {
-                        // Registration failed
-                        Toast.makeText(
-                                context,
-                                "Registration failed!!"
-                                        + " Please try again later",
-                                Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
+        //TODO: Error Checking for Email and Password Syntax
+        if(user.isEmpty() || pass.isEmpty())
+        {
+            Toast.makeText(getContext(), "Email or Password field cannot be empty. ", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            if(pass.equals(passConfirm)) {
+
+                firebaseAuth
+                        .createUserWithEmailAndPassword(user, pass)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(context,
+                                        "Registration successful!",
+                                        Toast.LENGTH_LONG)
+                                        .show();
+
+                                // if the user created intent to login activity
+                                Intent intent
+                                        = new Intent(context,
+                                        MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // Registration failed
+                                Toast.makeText(
+                                        context,
+                                        "Registration failed."
+                                                + " Please try again later",
+                                        Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        });
+            }
+            else
+            {
+                Toast.makeText(getContext(), "Passwords do not match.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -102,5 +127,8 @@ public class SignupTab extends Fragment {
     @Override public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    public interface OnFragmentInteractionListener {
     }
 }
