b5fa4f87cbe5dfd6766f8c0f9a843a3015e23c99
Anil Poudel <anil.pdl12@gmail.com>
2020-03-18 15:03:57 -0500

    Viewpager bugfix and Logo layout fix.

--- app/src/main/java/com/csce4901/tnma/MainActivity.java
+++ app/src/main/java/com/csce4901/tnma/MainActivity.java
@@ -2,10 +2,14 @@ package com.csce4901.tnma;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.csce4901.tnma.Connector.FirebaseConnector;
 import com.google.android.material.tabs.TabLayout;
@@ -14,28 +18,78 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity implements LoginTab.OnFragmentInteractionListener, SignupTab.OnFragmentInteractionListener{
 
     private static Context context;
     ImageView tnmaLogo;
     FirebaseConnector fbConnector = new FirebaseConnector();
 
+    @SuppressLint("ShowToast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
+
+
         //set logo dynamically
         retrieveDynamicLogoFromDB();
+        //set tabs
+        setupTabs();
+    }
 
-        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
-        tabLayout.addTab(tabLayout.newTab().setText("Login"));
-        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+
+
+    public static Context getAppContext() {
+        return MainActivity.context;
+    }
+
+    protected void retrieveDynamicLogoFromDB(){
+        tnmaLogo = findViewById(R.id.tnmaLogo);
+        fbConnector.firebaseSetup();
+        FirebaseFirestore db = fbConnector.getDb();
+        DocumentReference docRef = db.collection("dynamic-logo").document("TNMA_LOGO");
+        docRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                assert document != null;
+                if (document.exists()) {
+                    Log.d(MainActivity.class.getName(), "DocumentSnapshot data: " + document.getData());
+                    Picasso.get()
+                            .load(Objects.requireNonNull(document.get("logo")).toString())
+                            .fit()
+                            .into(tnmaLogo);
+                } else {
+                    Log.d(MainActivity.class.getName(), "No such document");
+                }
+            } else {
+                Log.d(MainActivity.class.getName(), "get failed with ", task.getException());
+            }
+        });
+    }
+
+    public boolean hasInternet() throws InterruptedException, IOException {
+        final String command = "ping -c 1 google.com";
+        return Runtime.getRuntime().exec(command).waitFor() == 0;
+    }
+
+
+    private void setupTabs(){
+        //Login/Signup Tabs
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final ViewPager viewPager = findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        tabLayout.setupWithViewPager(viewPager);
         viewPager.setAdapter(adapter);
+
+        tabLayout.getTabAt(0).setText("Login");
+        tabLayout.getTabAt(1).setText("Signup");
+
         viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -52,34 +106,6 @@ public class MainActivity extends AppCompatActivity implements LoginTab.OnFragme
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        });
-
-        }
-
-    public static Context getAppContext() {
-        return MainActivity.context;
-    }
-
-    protected void retrieveDynamicLogoFromDB(){
-        tnmaLogo = findViewById(R.id.tnmaLogo);
-        fbConnector.firebaseSetup();
-        FirebaseFirestore db = fbConnector.getDb();
-        DocumentReference docRef = db.collection("dynamic-logo").document("TNMA_LOGO");
-        docRef.get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                DocumentSnapshot document = task.getResult();
-                if (document.exists()) {
-                    Log.d(MainActivity.class.getName(), "DocumentSnapshot data: " + document.getData());
-                    Picasso.get()
-                            .load(document.get("logo").toString())
-                            .resize(158,158)
-                            .into(tnmaLogo);
-                } else {
-                    Log.d(MainActivity.class.getName(), "No such document");
-                }
-            } else {
-                Log.d(MainActivity.class.getName(), "get failed with ", task.getException());
             }
         });
     }
