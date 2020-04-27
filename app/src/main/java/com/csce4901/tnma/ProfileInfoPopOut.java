61079de32e05261e948aa20602f6803f06200aff
Abiral <abiral7@gmail.com>
2020-04-27 12:04:24 -0500

    database integration on update info from profile

--- app/src/main/java/com/csce4901/tnma/ProfileInfoPopOut.java
+++ app/src/main/java/com/csce4901/tnma/ProfileInfoPopOut.java
@@ -2,13 +2,12 @@ package com.csce4901.tnma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.csce4901.tnma.DAO.GeneralUserDao;
 import com.csce4901.tnma.DAO.Impl.GeneralUserDaoImpl;
@@ -42,14 +41,29 @@ public class ProfileInfoPopOut extends AppCompatActivity {
         String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
         GeneralUserDao user = new GeneralUserDaoImpl();
-        user.getUserProfileInfo2(email, profilefName, profilelName, profileName, profilePhone, profileCity, profileState);
-
+        user.getUserProfileEditInfo(email, profilefName, profilelName, profileName, profilePhone, profileCity, profileState);
         Drawable img = getApplicationContext().getResources().getDrawable(R.drawable.tnma);
         avatar.setImageDrawable(img);
 
         changeInfo.setOnClickListener(v -> {
-            //change values in the database here- using profilefName, profilelName, profilePhone, profileCity, profileState
-            //profileName is not editable= can only change first and last name
+            GeneralUserDao generalUserDao = new GeneralUserDaoImpl();
+            if(isNotEmpty(profilefName) && isNotEmpty(profilelName) && isNotEmpty(profilePhone)
+                    && isNotEmpty(profileCity) && isNotEmpty(profileState)) {
+                generalUserDao.updateUserProfileInfo(email, profilefName.getText().toString(),
+                        profilelName.getText().toString(),
+                        profilePhone.getText().toString(),
+                        profileCity.getText().toString(),
+                        profileState.getText().toString());
+                Toast.makeText(getBaseContext(), "User info updated successfully", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getBaseContext(), "Cannot update empty field", Toast.LENGTH_LONG).show();
+            }
         });
+    }
+
+    private boolean isNotEmpty(TextView etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return true;
+        return false;
     }
 }
