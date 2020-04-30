b4b87fa373a01db222edcdea8832210250210ec5
Abiral <abiral7@gmail.com>
2020-04-30 11:17:56 -0500

    Added null check before getting count of student list from mentor & code cleanup

--- app/src/main/java/com/csce4901/tnma/DAO/ChatDao.java
+++ app/src/main/java/com/csce4901/tnma/DAO/ChatDao.java
@@ -2,5 +2,4 @@ package com.csce4901.tnma.DAO;
 
 public interface ChatDao {
     public void storeNewMessage(String dt, String sender, String receiver, String message);
-    public void retrieveMessages(String email);
 }
